8187da79d7081c5cc878fbc5bee30b62bccea7f6
Tomasz Długosz <tomek3d@gmail.com>
2011-08-14 18:13:57 +0200

    introducing css and doctype

--- project_b.py
+++ project_b.py
@@ -14,6 +14,14 @@ from lxml import html
 
 oldTes=[u'Rdz', 'Wj', 'Kp³', 'Lb', 'Pwt', 'Joz', 'Sdz', 'Rt', '1 Sm', '2 Sm', '1 Krl', '2 Krl', '1 Krn', '2 Krn', 'Ezd', 'Ne', 'Tb', 'Jdt', 'Est', '1 Mch', '2 Mch', 'Hi', 'Ps', 'Prz', 'Koh', 'Pnp', 'Mdr', 'Syr', 'Iz', 'Jr', 'Lm', 'Ba', 'Ez', 'Dn', 'Oz', 'Jl', 'Am', 'Ab', 'Jon', 'Mi', 'Na', 'Ha', 'So', 'Ag', 'Za', 'Ml']
 newTes=[u'Mt', 'Mk', '£k', 'J', 'Dz', 'Rz', '1 Kor', '2 Kor', 'Ga', 'Ef', 'Flp', 'Kol', '1 Tes', '2 Tes', '1 Tm', '2 Tm', 'Tt', 'Flm', 'Hbr', 'Jk', '1 P', '2 P', '1 J', '2 J', '3 J', 'Jud', 'Ap']
+css = '''
+<style type="text/css">
+    .miedzytytul1 {font-weight:bold; color:#0099cf; text-align:center; font-size:14px;}
+    .przypis {color:#0000ff; font-weight:bold; font-size:11px;}
+    .werset {font-weight:bold; font-size:15px; color:#000000;}
+</style>
+'''
+doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" />'
 
 class Book:
     def __init__(self):
@@ -85,6 +93,10 @@ class Book:
 
 test = Book()
 test.GetBook('Ag')
+print doctype
 print "<html>"
+print "<head>"
+print css
+print "</head>"
 test.PrintBookContent()
 print "</html>"
