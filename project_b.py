e01bd0ffeac2355a72224b2c0baaa25a497bcaf9
Tomasz Długosz <tomek3d@gmail.com>
2011-08-14 21:20:22 +0200

    meta tag with encoding

--- project_b.py
+++ project_b.py
@@ -21,6 +21,7 @@ css = '''
 </style>
 '''
 doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" >'
+meta = '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" >'
 title = '<title>Pismo Święte</title>'
 
 class Book:
@@ -94,6 +95,7 @@ test.GetBook('Ag')
 print doctype
 print "<html>"
 print "<head>"
+print meta
 print title
 print css
 print "</head>"
